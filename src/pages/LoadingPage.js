1da1511255f14422dd405654c64bbc41ab887f02
chessering <yi9353@naver.com>
2023-09-07 05:21:11 +0900

    Feat: Add Loading Page

---
+++ src/pages/LoadingPage.js
@@ -0,0 +1,10 @@
+import "../components/LoadingPage.css"
+
+function LoadingPage() {
+    return(
+      <div className="LoadingPage">
+        <span className="Loader"></span>
+      </div>  
+    );
+}
+export default LoadingPage;
